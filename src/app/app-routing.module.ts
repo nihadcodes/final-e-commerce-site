fafbaf9ef6ba220fcfd6d053debef5707508e1d3
nihadcodes <nihad.exe@gmail.com>
2023-11-25 01:26:25 +0600

    structure added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerComponent } from './seller/seller.component';
+
+const routes: Routes = [
+{
+  path:'',
+  component:HomeComponent,
+},
+
+{
+path:'seller',
+  component:SellerComponent,
+},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
