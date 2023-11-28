7d15038ca8d9a273a9955e52d6dbee4e4ac0b5bc
nihadcodes <nihad.exe@gmail.com>
2023-11-28 22:26:23 +0600

    cart initialized

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+constructor(private route: Router) {
+
+}
+
+ngOninit():void {
+  this.route.events.subscribe((val:any)=> {
+    if(val.url) {
+console.warn(val.url)
+      if(localStorage.getItem('seller')&& val.url.includes('seller')) {
+        console.warn("in seller area") 
+      }
+      
+        else {
+
+          console.warn("outside seller")
+        }
+      
+    } 
+  })
+}
 
 }
